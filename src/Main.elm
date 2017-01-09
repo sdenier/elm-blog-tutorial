6707c45607f3f6dfc7d21416f8e017ffb1d66606
Simon Denier <simon@sogilis.com>
2017-01-09 18:45:25 +0100

    Walking skeleton for elm+navigation project

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,57 @@
 module Main exposing (..)
 
+import Navigation
 import Html exposing (..)
 
 
-main : Html msg
+-- MAIN
+
+
+main : Program Never State Msg
 main =
+    Navigation.program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
+
+
+
+-- MODEL
+
+
+type alias State =
+    {}
+
+
+initialState : State
+initialState =
+    {}
+
+
+init : Navigation.Location -> ( State, Cmd Msg )
+init location =
+    ( initialState, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = UrlChange Navigation.Location
+
+
+update : Msg -> State -> ( State, Cmd Msg )
+update msg state =
+    ( state, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : State -> Html Msg
+view model =
     text "Hello World"
